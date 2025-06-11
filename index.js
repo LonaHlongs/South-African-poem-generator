ae857363af88282180dc1b89d0bbd6602fb901a3
LonaHlongs <unathihlongwana@gmail.com>
2025-06-11 15:36:52 +0200

    Improved the User Experience

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ function displayPoetry(response) {
 
 }
 
-
-
 function generatePoetry(event) {
   event.preventDefault();
 
@@ -18,7 +16,11 @@ function generatePoetry(event) {
   let prompt = `Generate a South African poem about ${formEntryInput.value}`;
   let context = "You are a cultural South African poet. You write short, 4 line, poems that reflect the beauty, diversity, and spirit of South Africa. Your poems are rich in imagery and emotion, capturing the essence of the country's landscapes, people, and traditions. The poem must be in basic HTML. Do NOT add HTML to the poem itself.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poetryElement = document.querySelector("#poetry");
+  poetryElement.classList.remove("hidden");
+  poetryElement.innerHTML = "Just a few seconds, generating your poem...please wait😁";
   
+
   axios.get(apiUrl).then(displayPoetry);
   
 }
